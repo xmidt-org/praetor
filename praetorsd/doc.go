ced0db4fd7f9e3e15475a1fabd332000c814ce02
johnabass <johnbass1017@yahoo.com>
2023-10-25 13:49:58 -0700

    chore: initial package

---
+++ praetorsd/doc.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2023 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+Package praetorsd provides service discovery using consul within a
+go.uber.org/fx application.
+*/
+package praetorsd
