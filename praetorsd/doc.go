3d7388433986fc2837bae95afbcd285066206396
johnabass <johnbass1017@yahoo.com>
2025-01-03 13:41:08 -0800

    pruned this for now

--- praetorsd/doc.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Comcast Cable Communications Management, LLC
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-Package praetorsd provides service registration and discovery within an fx
-application.  The praetor package provides a way of bootstrapping a consul
-client, but that package is not required for this package to be useful.
-*/
-package praetorsd
