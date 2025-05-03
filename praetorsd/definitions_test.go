4781dc8ecc65b116f7a934dfcdaf24366dc97fe7
johnabass <johnbass1017@yahoo.com>
2025-05-02 19:01:18 -0700

    chore: temporarily removed tests

--- praetorsd/definitions_test.go
+++ praetorsd/definitions_test.go
@@ -2,25 +2,3 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package praetorsd
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/suite"
-)
-
-type ServiceRegistrationChecksTestSuite struct {
-	suite.Suite
-}
-
-func TestServiceRegistrationChecks(t *testing.T) {
-	suite.Run(t, new(ServiceRegistrationChecksTestSuite))
-}
-
-type RegistrationsBuilderTestSuite struct {
-	suite.Suite
-}
-
-func TestRegistrationsBuilder(t *testing.T) {
-	suite.Run(t, new(RegistrationsBuilderTestSuite))
-}
